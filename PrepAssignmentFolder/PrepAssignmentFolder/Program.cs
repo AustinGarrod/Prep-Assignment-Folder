23cb86f60e45728512c152046722fd176758800e
Austin Garrod <austin@austingarrod.ca>
2021-02-24 15:35:39 -0500

    Documented code

--- PrepAssignmentFolder/PrepAssignmentFolder/Program.cs
+++ PrepAssignmentFolder/PrepAssignmentFolder/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * Main program functionality to handle proceessing of student file submissions
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,10 +16,11 @@ namespace PrepAssignmentFolder
     {
         static void Main(string[] args)
         {
-            bool waitForKey = true;
-            bool processInput = true;
-            string path = "";
+            bool waitForKey = true;     // If we need to wait for a keypress, used to end program
+            bool processInput = true;   // If we have a valid directory to process input
+            string path = "";           // Path of directory to process, where zipped student submissions are
 
+            // Ask user for directory to process, keep asking until value provided
             while (processInput)
             {
                 string input;
@@ -23,69 +28,69 @@ namespace PrepAssignmentFolder
                 Console.WriteLine("Please enter a directory to process:");
                 input = Console.ReadLine().Trim();
 
-                if (input != "")
+                if (input != "") // Path provided
                 {
-                    path = input;
-                    processInput = false;
+                    path = input;   // update out path
+                    processInput = false;  // Exit the loop
                 }
             }
             
-
+            // If the user entered a valid path
             if (Directory.Exists(path))
             {
-                string[] files;
-                List<Submission> submissions = new List<Submission>();
+                string[] files; // Array to hold submission file paths from user provided directory
+                List<Submission> submissions = new List<Submission>(); // List to hold submission objects that contain info on student submissions
 
-                if (File.Exists(path + @"\index.html"))
+                if (File.Exists(path + @"\index.html")) // Check to see if D2L gave us an index file
                 {
-                    File.Delete(path + @"\index.html");
+                    File.Delete(path + @"\index.html"); // Delete the D2L index file
                 }
 
-                files = Directory.GetFiles(path);
+                files = Directory.GetFiles(path); // Get all the files from the provided submission directory
 
-                for (int fileCount = 0; fileCount < files.Length; fileCount++)
+                for (int fileCount = 0; fileCount < files.Length; fileCount++) // Loop through all the files found in the folder
                 {
-                    Submission newSubmission = new Submission(files[fileCount]);
+                    Submission newSubmission = new Submission(files[fileCount]); // Create a new submission object for the submission zip
 
-                    if (submissions.Count > 0 && submissions[submissions.Count - 1].StudentName == newSubmission.StudentName)
+                    if (submissions.Count > 0 && submissions[submissions.Count - 1].StudentName == newSubmission.StudentName) // Check to see if the user has submitted a file before this one
                     {
-                        if (submissions[submissions.Count - 1].SubmissionDateTime < newSubmission.SubmissionDateTime)
+                        if (submissions[submissions.Count - 1].SubmissionDateTime < newSubmission.SubmissionDateTime) // Confirm that this file is newer than the previous submission
                         {
-                            File.Delete(submissions[submissions.Count - 1].FilePath);
-                            submissions[submissions.Count - 1] = newSubmission;
+                            File.Delete(submissions[submissions.Count - 1].FilePath); // Delete the older submission
+                            submissions[submissions.Count - 1] = newSubmission; // Replace that submission in the list with the latest submission found
                         }
-                        else
+                        else // The original file was newer
                         {
-                            File.Delete(newSubmission.FilePath);
+                            File.Delete(newSubmission.FilePath); // delete the older of the two submissions
                         }
                     }
-                    else
+                    else // No multiple submissions found
                     {
-                        submissions.Add(newSubmission);
+                        submissions.Add(newSubmission); // add the submission to the list
                     }
                 }
 
                 
 
-                foreach (Submission submission in submissions)
+                foreach (Submission submission in submissions) // Loop through the cleaned list of submissions
                 {
-                    using (ArchiveFile archiveFile = new ArchiveFile(submission.FilePath))
+                    using (ArchiveFile archiveFile = new ArchiveFile(submission.FilePath)) // Load the zip of the submission for handling
                     {
                         Console.WriteLine("Extracting " + submission.FileName);
-                        archiveFile.Extract(path + @"\" + submission.StudentName); // extract all
+                        archiveFile.Extract(path + @"\" + submission.StudentName); // extract files to folder named after student
                     }
-                    File.Delete(submission.FilePath);
+                    File.Delete(submission.FilePath); // Delete the original zip file
                 }
 
                Console.WriteLine("Done");
             }
-            else
+            else // User entered an invalid path
             {
                 Console.WriteLine("Directory: " + path + " does not exist");
-                waitForKey = true;
+                waitForKey = true; // Wait for keypress so user can see error
             }
 
-            if (waitForKey) Console.ReadKey();
+            if (waitForKey) Console.ReadKey(); // Used to keep console open so user can see errors
 
         }
     }
--- PrepAssignmentFolder/PrepAssignmentFolder/Submission.cs
+++ PrepAssignmentFolder/PrepAssignmentFolder/Submission.cs
@@ -7,45 +7,68 @@ using System.Threading.Tasks;
 
 namespace PrepAssignmentFolder
 {
+    /// <summary>
+    /// Class to hold data on submissions
+    /// </summary>
     class Submission
     {
+        // Property to hold original file name
         private string _originalFilePath;
 
+        // Accessor for file name
         public string FileName
         {
             get { return Path.GetFileName(_originalFilePath); }
         }
 
+        // Accessor for student name
         public string StudentName
         {
             get { return ExtractStudentName(this.FileName); }
         }
 
+        // Accessor for submission date and time
         public DateTime SubmissionDateTime
         {
             get { return ExtractSubmissionDateTime(this.FileName); }
         }
 
+        // Accessor for file extension
         public string FileExtension
         {
             get { return Path.GetExtension(this._originalFilePath); }
         }
 
+        // Accessor for original file path
         public string FilePath
         {
             get { return this._originalFilePath; }
         }
 
+        /// <summary>
+        /// Constructor for submission object
+        /// </summary>
+        /// <param name="path">File path of submission</param>
         public Submission(string path)
         {
             this._originalFilePath = path;
         }
 
+        /// <summary>
+        /// Extracts student name from submission file name
+        /// </summary>
+        /// <param name="fileString">filename to be processed$</param>
+        /// <returns>Student name as string</returns>
         public static string ExtractStudentName(string fileString)
         {
             return fileString.Split(new string[] { " - " }, StringSplitOptions.None)[1].Trim();
         }
 
+        /// <summary>
+        /// Extracts submission date and time from submission file name
+        /// </summary>
+        /// <param name="fileString">filename to be processed$</param>
+        /// <returns>Submission date and time as DateTime</returns>
         public static DateTime ExtractSubmissionDateTime(string fileString)
         {
             string dateTimeString = fileString.Split(new string[] { " - " }, StringSplitOptions.None)[2].Trim();
